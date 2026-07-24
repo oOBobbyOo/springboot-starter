d3b7e2f29ec26ae20adf7011e01c979d08cbb554
oOBobbyOo <944831575@qq.com>
2026-07-24 15:35:55 +0800

    refactor: test println

--- src/test/java/com/company/springbootstarter/SpringbootStarterApplicationTests.java
+++ src/test/java/com/company/springbootstarter/SpringbootStarterApplicationTests.java
@@ -1,13 +1,23 @@
 package com.company.springbootstarter;
 
+import com.company.springbootstarter.controller.HelloController;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class SpringbootStarterApplicationTests {
 
+    @Resource
+    private ApplicationContext ctx;
+
     @Test
     void contextLoads() {
+        System.out.println(ctx);
+
+         HelloController helloController =  ctx.getBean(HelloController.class);
+         System.out.println("helloController = " + helloController);
     }
 
 }
